78fff3f030538d40f360fe29466497b551dda071
sunghyun JE <khaos1004@naver.com>
2025-02-24 15:02:05 +0900

    fix

--- mok_server_std.js
+++ mok_server_std.js
@@ -203,6 +203,8 @@ app.post(resultUri, async (req, res) => {
         clientTxId = decryptMOKResultObject.clientTxId;
     }
     // 세션 내 요청 clientTxId 와 수신한 clientTxId 가 동일한지 반드시 비교
+    console.log("req.session.clientTxId: **" + req.session.clientTxId)
+    console.log("clientTxId: **" + clientTxId)
     if (req.session.clientTxId != clientTxId) {
         return res.send('-4|세션값에 저장된 거래ID 비교 실패');
     }
